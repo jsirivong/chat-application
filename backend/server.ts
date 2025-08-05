f6a2a3595a932b5fa6a81f6541d453d8c950b33b
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-05 18:18:12 -0400

    initial comimt

---
+++ backend/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from 'cors';
+import morgan from 'morgan'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on localhost:${PORT}\nServer is listening on port ${PORT}`);
+}).on("error", (err)=>{
+    console.log("Server start was unsuccessful.\n", err);
+})
