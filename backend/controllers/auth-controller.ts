b5f2f3dd2667e1d3ce21ab4a42a6ca9557bf99a6
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-05 21:27:52 -0400

    add controller and router for readability

---
+++ backend/controllers/auth-controller.ts
@@ -0,0 +1,13 @@
+import { type Request, type Response } from 'express';
+
+export const signup = async (req: Request, res: Response) => {
+    
+}
+
+export const login = async (req: Request, res: Response) => {
+
+}
+
+export const logout = (req: Request, res: Response) => {
+    
+}
--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import morgan from 'morgan'
 import dotenv from 'dotenv';
+import auth from "./routes/auth-route.ts";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use("/api/v1/auth", auth)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on localhost:${PORT}\nServer is listening on port ${PORT}`);
