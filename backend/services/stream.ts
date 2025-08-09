6d2302495bc2d24734f63e44de1c2a7d9ebb2aff
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-09 17:09:44 -0400

    create stream client and add user to the stream client

---
+++ backend/services/stream.ts
@@ -0,0 +1,28 @@
+import { StreamChat, type UserResponse } from 'stream-chat';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const API_KEY = process.env.STREAM_API_KEY;
+const API_SECRET = process.env.STREAM_API_SECRET;
+
+if (!API_KEY || !API_SECRET){
+    throw new Error("Stream API key or secret is missing");
+}
+
+const streamClient = StreamChat.getInstance(API_KEY, API_SECRET);
+
+// creates/updates a new Stream user
+// creates if the user does not exist, update if it does
+export const upsertStreamUser = async (userData: UserResponse) => {
+    try {
+        await streamClient.upsertUsers([userData]);
+        return userData
+    } catch (err) {
+        console.error("Error upserting Stream user.\n", err);
+    }
+};
+
+export const generateStreamToken = (id: number) => {
+    
+};
