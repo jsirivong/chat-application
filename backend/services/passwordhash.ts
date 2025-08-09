23283cc1f2fb2369d2504c9f8b58a6311727d10b
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-09 04:51:19 -0400

    added comparePassword function

--- backend/services/passwordhash.ts
+++ backend/services/passwordhash.ts
@@ -1,10 +1,12 @@
 import bcrypt from 'bcrypt';
 
-const hashPassword = async (password: string): Promise<string> => {
+export const hashPassword = async (password: string): Promise<string> => {
     // in order to create a secure password in the database, we have to utilize two algorithms: salting and password hashing
     const saltRounds = 10;
     const hash: string = await bcrypt.hash(password, saltRounds);
     return hash;
 };
 
-export default hashPassword;
+export const comparePassword = async (enteredPassword: string, hashedPassword: string): Promise<boolean> => {
+    return await bcrypt.compare(enteredPassword, hashedPassword);
+}
