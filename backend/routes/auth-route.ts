c5a4753b5b9329a4a0161924f516ffbc0cf1a260
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-08 21:50:41 -0400

    change sign-up to register

--- backend/routes/auth-route.ts
+++ backend/routes/auth-route.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { login, signup, logout } from '../controllers/auth-controller.ts';
+import { login, register, logout } from '../controllers/auth-controller.ts';
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout)
 
