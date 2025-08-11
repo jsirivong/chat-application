e201041bddfff8c4f8564641d5669bb435399856
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-10 20:21:51 -0400

    finish /complete and /me routes

--- backend/routes/auth-route.ts
+++ backend/routes/auth-route.ts
@@ -1,10 +1,17 @@
-import express from 'express';
-import { login, register, logout } from '../controllers/auth-controller.ts';
+import express, { type Response } from 'express';
+import { login, register, logout, completeProfile } from '../controllers/auth-controller.ts';
+import { authorizeUser } from '../middleware/authorize.user.ts';
+import type RequestUser from '../types/RequestUser.ts';
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout)
+router.post("/complete", authorizeUser, completeProfile)
+
+router.get("/me", authorizeUser, (req: RequestUser, res: Response)=>{
+    res.status(200).json({success: true, data: req.user})
+})
 
 export default router;
