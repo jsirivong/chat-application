b572b1a27c240e3d9e0c492dd66ba97fa4155459
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-10 20:58:38 -0400

    add chat router

---
+++ backend/routes/chat-route.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { authorizeUser } from '../middleware/authorize.user.ts';
+import { getStreamToken } from '../controllers/chat-controller.ts'
+
+
+const router = express.Router();
+
+router.get("/token", authorizeUser, getStreamToken)
+
+export default router;
