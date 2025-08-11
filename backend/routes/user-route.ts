9f6beca2d68571fae237b9635b73355eeb36a62b
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-10 20:22:13 -0400

    implement user router for user restful api

---
+++ backend/routes/user-route.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { getRecommendedUsers, getFriends, sendFriendRequest, acceptFriendRequest, getFriendRequests, getOutgoingFriendRequests } from '../controllers/user-controller.ts'
+import { authorizeUser } from '../middleware/authorize.user.ts'
+ 
+const router = express.Router();
+
+// apply auth middleware to all requests/routes
+router.use(authorizeUser)
+
+router.post("/friend-request/:id", sendFriendRequest)
+router.post("/friend-request/:id/accept", acceptFriendRequest);
+router.get("/", getRecommendedUsers)
+router.get("/friends", getFriends)
+router.get("/friend-requests", getFriendRequests)
+router.get("/outgoing-friend-requests", getOutgoingFriendRequests)
+
+export default router; 
